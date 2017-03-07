7678c174d1856ab1d68cdc44d61f97c3f73718b8
MakeHui <makehuir@gmail.com>
2017-03-07 13:32:02 +0800

    add eslint

--- assets/script/Controller/GameRoomScene.js
+++ assets/script/Controller/GameRoomScene.js
@@ -9,7 +9,7 @@ cc.Class({
          */
         handCardPrefabs: {
             default: [],
-            type: cc.Prefab
+            type: cc.Prefab,
         },
 
         handCardDistrict: {
@@ -23,7 +23,7 @@ cc.Class({
          */
         dirtyCardPrefabs: {
             default: [],
-            type: cc.Prefab
+            type: cc.Prefab,
         },
 
         dirtyCardDistrict: {
@@ -40,13 +40,13 @@ cc.Class({
         // 明杠
         exposedPrefab: {
             default: [],
-            type: cc.Prefab
+            type: cc.Prefab,
         },
-        
+
         // 暗杠
         concealedKongPrefab: {
             default: [],
-            type: cc.Prefab
+            type: cc.Prefab,
         },
 
         pongKongChowDistrict: {
@@ -71,12 +71,12 @@ cc.Class({
 
         roomInfo: {
             default: [],
-            type: cc.Label   
+            type: cc.Label,
         },
 
         actionPanel: {
             default: [],
-            type: cc.Node
+            type: cc.Node,
         },
 
         waitPanel: cc.Node,
@@ -98,24 +98,24 @@ cc.Class({
         handCardIsSelected: 0,
     },
 
-    onLoad: function () {
-        this.wsUrl = 'ws://' + Global.tempCache.getServerIp() + ':' + Global.tempCache.getServerPort() + '/ws';
+    onLoad() {
+        this.wsUrl = `ws://${Global.tempCache.getServerIp()}:${Global.tempCache.getServerPort()}/ws`;
         this.roomId = Global.tempCache.getRoomId();
-        let self = this;
-        let scriptName = 'GameRoomScene';
+        const self = this;
+        const scriptName = 'GameRoomScene';
 
         WebSocketManager.ws.openSocket(this.wsUrl);
-        WebSocketManager.ws.addOnopenListener(scriptName, function(evt) {
-            WebSocketManager.sendMessage('EnterRoom', {roomId: self.roomId});
+        WebSocketManager.ws.addOnopenListener(scriptName, (evt) => {
+            WebSocketManager.sendMessage('EnterRoom', { roomId: self.roomId });
         });
-        WebSocketManager.ws.addOnmessageListener(scriptName, function(evt, commandName, result) {
-            self['on' + commandName + 'Message'](result);
+        WebSocketManager.ws.addOnmessageListener(scriptName, (evt, commandName, result) => {
+            self[`on${commandName}Message`](result);
         });
-        WebSocketManager.ws.addOnerrorListener(scriptName, function (evt) {
-            
+        WebSocketManager.ws.addOnerrorListener(scriptName, (evt) => {
+
         });
-        WebSocketManager.ws.addOncloseListener(scriptName, function (evt) {
-            
+        WebSocketManager.ws.addOncloseListener(scriptName, (evt) => {
+
         });
 
         this.emojiNode = cc.Node;
@@ -124,21 +124,11 @@ cc.Class({
         this.fastChatPanelPosition = this.fastChatPanel.position;
         this.menuPanelPosition = this.menuPanel.position;
 
-        this.audio = window.Tools.audioEngine.init();
-
-        // this._appendCardToHandCardDistrict(0, [0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
-        // this._appendCardToHandCardDistrict(1, [0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
-        // this._appendCardToHandCardDistrict(2, [0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
-        // this._appendCardToHandCardDistrict(3, [0,1,2,3,4,5,6,7,8,9,10,11,12,13]);
-        //
-        // this._appendCardToPongKongChowDistrict(0, {type:"gang"});
-        // this._appendCardToPongKongChowDistrict(1, {type:"gang"});
-        // this._appendCardToPongKongChowDistrict(2, {type:"gang"});
-        // this._appendCardToPongKongChowDistrict(3, {type:"gang"});
+        this.audio = Tools.audioEngine.init();
     },
 
-    update: function(dt) {
-        this.roomInfo[0].string = Tools.formatDatetime("hh:ii:ss");
+    update(dt) {
+        this.roomInfo[0].string = Tools.formatDatetime('hh:ii:ss');
 
         // 每次聊天的冷却时间
         if (+new Date() > Global.fastChatShowTime + this.fastChatShowTime) {
@@ -148,75 +138,75 @@ cc.Class({
         }
 
         if (this.fastChatProgressBar.progress <= 1.0 && this.fastChatProgressBar.progress >= 0) {
-            this.fastChatProgressBar.progress -= dt * window.Global.fastChatWaitTime;
+            this.fastChatProgressBar.progress -= dt * Global.fastChatWaitTime;
         }
 
         if (this.voiceProgressBar.progress <= 1.0 && this.voiceProgressBar.progress >= 0) {
-            this.voiceProgressBar.progress -= dt * window.Global.fastChatWaitTime;
+            this.voiceProgressBar.progress -= dt * Global.fastChatWaitTime;
         }
     },
 
-    /******************************************************************************************************************
+    /** ***************************************************************************************************************
      *                                       socket on message
      ******************************************************************************************************************/
 
-    onEnterRoomMessage: function (data) {
-        if (data.getCode() == 1) {
+    onEnterRoomMessage(data) {
+        if (data.getCode() === 1) {
             let kwargs = JSON.parse(data.getKwargs());
             let restCards = data.getRestCards();
             let playerList = data.getPlayerList();
-            for (let i = 0; i < playerList.length; ++i) {
+            for (let i = 0; i < playerList.length; i += 1) {
                 cc.log(playerList[i].getPlayerUuid());
             }
         }
     },
 
-    /******************************************************************************************************************
+    /** ***************************************************************************************************************
      *                                       button on click
      ******************************************************************************************************************/
 
-    wechatInviteOnClick: function() {
-        window.Tools.captureScreen(this.node, function(filePath) {
+    wechatInviteOnClick() {
+        Tools.captureScreen(this.node, (filePath) => {
             cc.log(filePath);
         });
     },
 
-    openFastChatPanelOnClick: function(event, data) {
+    openFastChatPanelOnClick() {
         if (this.fastChatProgressBar.progress <= 0) {
             if (this.fastChatPanel.position.x === this.fastChatPanelPosition.x) {
-                window.Animation.openPanel(this.fastChatPanel);
+                Animation.openPanel(this.fastChatPanel);
             }
             else {
-                window.Animation.closePanel(this.fastChatPanel);
+                Animation.closePanel(this.fastChatPanel);
             }
         }
     },
 
-    voiceOnClick: function() {
+    voiceOnClick() {
         if (this.voiceProgressBar.progress <= 0) {
             this.voiceProgressBar.progress = 1.0;
-            cc.log("voiceOnClick");
+            cc.log('voiceOnClick');
         }
     },
 
-    openMenuOnClick: function() {
+    openMenuOnClick() {
         if (this.menuPanel.position.x === this.menuPanelPosition.x) {
-            window.Animation.openPanel(this.menuPanel);
+            Animation.openPanel(this.menuPanel);
         }
         else {
-            window.Animation.closePanel(this.menuPanel);
+            Animation.closePanel(this.menuPanel);
         }
     },
 
-    closeDialogOnClick: function() {
+    closeDialogOnClick() {
         // 检查是否关闭聊天面板
         if (this.fastChatPanel.position.x !== this.fastChatPanelPosition.x) {
-            window.Animation.closePanel(this.fastChatPanel);
+            Animation.closePanel(this.fastChatPanel);
         }
 
         // 检查是否关闭菜单面板
         if (this.menuPanel.position.x !== this.menuPanelPosition.x) {
-            window.Animation.closePanel(this.menuPanel);
+            Animation.closePanel(this.menuPanel);
         }
 
         // 手牌复位
@@ -225,52 +215,52 @@ cc.Class({
         }
     },
 
-    switchFastChatPanelOnClick: function(evt, data) {
-        if (data == 1) {
-            this.fastChatPanel.getChildByName("fastChatView1").active = true;
-            this.fastChatPanel.getChildByName("fastChatView2").active =  false;
+    switchFastChatPanelOnClick(evt, data) {
+        if (data === 1) {
+            this.fastChatPanel.getChildByName('fastChatView1').active = true;
+            this.fastChatPanel.getChildByName('fastChatView2').active = false;
         }
         else {
-            this.fastChatPanel.getChildByName("fastChatView1").active = false;
-            this.fastChatPanel.getChildByName("fastChatView2").active = true;
+            this.fastChatPanel.getChildByName('fastChatView1').active = false;
+            this.fastChatPanel.getChildByName('fastChatView2').active = true;
         }
     },
 
-    wordChatOnClick: function(evt, data) {
+    wordChatOnClick(evt, data) {
         this.fastChatProgressBar.progress = 1.0;
-        this.audio.setAudioRaw(Global.audioResourcesUrl.fastChat["fw_male_" + data]).play();
+        this.audio.setAudioRaw(Global.audioResourcesUrl.fastChat[`fw_male_${data}`]).play();
 
-        window.Animation.closePanel(this.fastChatPanel);
+        Animation.closePanel(this.fastChatPanel);
     },
 
-    emojiChatOnClick: function(evt, data) {
+    emojiChatOnClick(evt, data) {
         this.fastChatProgressBar.progress = 1.0;
         this.fastChatShowTime = +new Date();
-        let self = this;
+        const self = this;
 
-        window.Tools.loadPrefab("emoji/emotion" + data, function(prefab) {
-            let node = cc.instantiate(prefab);
+        Tools.loadPrefab(`emoji/emotion${data}`, (prefab) => {
+            const node = cc.instantiate(prefab);
             self.emojiNode = node;
             self.node.addChild(node);
-            node.getComponent(cc.Animation).play("emotion" + data);
+            node.getComponent(cc.Animation).play(`emotion${data}`);
         });
 
-        window.Animation.closePanel(this.fastChatPanel);
+        Animation.closePanel(this.fastChatPanel);
     },
 
-    actionOnClick: function(event, data) {
+    actionOnClick(event, data) {
         this._hideActionPanel();
-        if (data != 0) {
+        if (data !== 0) {
             this.actionPanel[5].getComponent(cc.Sprite).spriteFrame = event.target.getComponent(cc.Sprite).spriteFrame;
             this._showActionPanel([5]);
         }
     },
 
-    selectedHandCardOnClick: function(event, data) {
+    selectedHandCardOnClick(event, data) {
         if (this.handCardIsSelected === data) {
             event.target.parent.destroy();
-            let node = cc.instantiate(this.dirtyCardPrefabs[0]);
-            let backgroundNode = node.getChildByName("background");
+            const node = cc.instantiate(this.dirtyCardPrefabs[0]);
+            // const backgroundNode = node.getChildByName('background');
             this.dirtyCardDistrict[0].addChild(node);
             return;
         }
@@ -280,14 +270,14 @@ cc.Class({
         this._resetHandCardPosition();
         event.target.setPositionY(24);
 
-        cc.log(event.target.parent.getChildByName("UserData").string);
+        cc.log(event.target.parent.getChildByName('UserData').string);
     },
 
-    closeOnClick: function() {
+    closeOnClick() {
         cc.director.loadScene('Lobby');
     },
 
-    /******************************************************************************************************************
+    /** ***************************************************************************************************************
      *                                       function
      ******************************************************************************************************************/
 
@@ -299,36 +289,36 @@ cc.Class({
      * 4: 胡
      * 5: 中间显示当前选择的动作, 0 除外
      */
-    _hideActionPanel: function() {
-        for (let i = 0; i < this.actionPanel.length; ++i) {
+    _hideActionPanel() {
+        for (let i = 0; i < this.actionPanel.length; i += 1) {
             this.actionPanel[i].active = false;
         }
     },
 
-    _showActionPanel: function(indexs) {
+    _showActionPanel(indexs) {
         if (indexs.length === 1) {
-            let self = this;
+            const self = this;
             self._hideActionPanel();
-            this.scheduleOnce(function() {
+            this.scheduleOnce(() => {
                 self._hideActionPanel();
             }, 1);
         }
 
-        for (let i = 0; i < indexs.length; ++i) {
+        for (let i = 0; i < indexs.length; i += 1) {
             this.actionPanel[indexs[i]].active = true;
         }
     },
 
-    _appendCardToHandCardDistrict: function(player, data) {
-        for (let i = 0; i < data.length; i++) {
-            let node = cc.instantiate(this.handCardPrefabs[player]);
+    _appendCardToHandCardDistrict(player, data) {
+        for (let i = 0; i < data.length; i += 1) {
+            const node = cc.instantiate(this.handCardPrefabs[player]);
             if (player === 0) {
-                let backgroundNode = node.getChildByName("background");
-                let clickEventHandler = window.Tools.createEventHandler(this.node, "GameRoomScene", "selectedHandCardOnClick", i);
+                const backgroundNode = node.getChildByName('background');
+                const clickEventHandler = Tools.createEventHandler(this.node, 'GameRoomScene', 'selectedHandCardOnClick', i);
                 backgroundNode.getComponent(cc.Button).clickEvents.push(clickEventHandler);
                 // todo: 数据组装
             }
-            node.getChildByName("UserData").string = "xxxxxx";
+            node.getChildByName('UserData').string = 'xxxxxx';
 
             this.handCardDistrict[player].addChild(node);
 
@@ -338,19 +328,19 @@ cc.Class({
         }
     },
 
-    _appendCardToPongKongChowDistrict: function(player, data) {
-        let index = player % 2;
+    _appendCardToPongKongChowDistrict(player, data) {
+        const index = player % 2;
         let node = cc.Node;
-        if(data.type === "gang") {
+        if (data.type === 'gang') {
             node = cc.instantiate(this.concealedKongPrefab[index]);
             // todo: 数据组装
         }
         this.pongKongChowDistrict[player].addChild(node);
     },
 
-    _resetHandCardPosition: function() {
-        for (let i = 0; i < this.handCardDistrict[0].childrenCount; ++i) {
-            this.handCardDistrict[0].children[i].getChildByName("background").setPositionY(0);
+    _resetHandCardPosition() {
+        for (let i = 0; i < this.handCardDistrict[0].childrenCount; i += 1) {
+            this.handCardDistrict[0].children[i].getChildByName('background').setPositionY(0);
         }
     },
 
