a5ab0afeb1c7caf1843a73fdbc30254c1cac5801
MakeHui <makehuir@gmail.com>
2017-04-10 14:08:21 +0800

    es6 to es5

--- assets/script/Manager/NativeExtensionManager.js
+++ assets/script/Manager/NativeExtensionManager.js
@@ -5,196 +5,208 @@ window.NativeExtensionManager = {
     _listener: {},
 
     _androidExtension: {
+        test: function getPasteboard() {
+            return jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'test', '()V');
+        },
+
         /**
          获取系统剪切板中的数据
          */
-        getPasteboard() {
+        getPasteboard: function getPasteboard() {
             return jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'getPasteboard', '()V');
         },
 
+
         /**
          开始录音
          */
-        startRecord() {
+        startRecord: function startRecord() {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'startRecord', '()V');
         },
+
 
         /**
          关闭录音
          */
-        stopRecord() {
+        stopRecord: function stopRecord() {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'stopRecord', '()V');
         },
+
 
         /**
          * 微信是否安装
          * @returns bool
          */
-        wechatIsWxAppInstalled() {
+        wechatIsWxAppInstalled: function wechatIsWxAppInstalled() {
             return jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'wechatIsWxAppInstalled', '()V');
         },
 
+
         /**
          微信分享土图片
-
          @param path 图片地址
          */
-        wechatImageShare(path) {
+        wechatImageShare: function wechatImageShare(path) {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'wechatImageShare:', '(Ljava/lang/String;)V', path);
         },
 
+
         /**
          微信链接分享
-
          @param link 链接
          @param title 标题
          @param description 说明
          */
-        wechatLinkShare(link, title, description) {
+        wechatLinkShare: function wechatLinkShare(link, title, description) {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'wechatLinkShare', '(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V', link, title, description);
         },
 
+
         /**
          初始化oss client
-
          @param endpoint 端点
          @param accessKey 访问key
          @param secretKey 访问秘钥
          */
-        ossInit(endpoint, accessKey, secretKey) {
+        ossInit: function ossInit(endpoint, accessKey, secretKey) {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'ossInit', '(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V', endpoint, accessKey, secretKey);
         },
 
+
         /**
          上传文件
-
          @param bucketName bucke 名称
          @param objectKey 存储到oss上的文件名
          @param file 本地文件路径
          */
-        ossUpload(bucketName, objectKey, file) {
+        ossUpload: function ossUpload(bucketName, objectKey, file) {
             jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'ossUpload', '(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V', bucketName, objectKey, file);
         },
+
 
         /**
          开始定位
          */
-        startLocation() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'startLocation', '()V');
+        startLocation: function startLocation() {
+            jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'startLocation', '()V');
         },
+
 
         /**
          检查网络是否通畅
-
          @return bool
          */
-        checkNetwork() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'checkNetwork', '()V');
-        },
+        checkNetwork: function checkNetwork() {
+            jsb.reflection.callStaticMethod('org/cocos2dx/javascript/AppActivity', 'checkNetwork', '()V');
+        }
     },
 
     _iOSExtension: {
+        test: function test() {
+            return jsb.reflection.callStaticMethod('MHCocosExtension', 'test');
+        },
 
         /**
          获取系统剪切板中的数据
          */
-        getPasteboard() {
-            return jsb.reflection.callStaticMethod('MHCocosExtension', 'getPasteboard:');
+        getPasteboard: function getPasteboard() {
+            return jsb.reflection.callStaticMethod('MHCocosExtension', 'getPasteboard');
         },
+
 
         /**
          开始录音
          */
-        startRecord() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'startRecord:');
+        startRecord: function startRecord() {
+            jsb.reflection.callStaticMethod('MHCocosExtension', 'startRecord');
         },
+
 
         /**
          关闭录音
          */
-        stopRecord() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'stopRecord:');
+        stopRecord: function stopRecord() {
+            jsb.reflection.callStaticMethod('MHCocosExtension', 'stopRecord');
         },
+
 
         /**
          * 微信是否安装
          * @returns bool
          */
-        wechatIsWxAppInstalled() {
-            return jsb.reflection.callStaticMethod('MHCocosExtension', 'wechatIsWxAppInstalled:');
+        wechatIsWxAppInstalled: function wechatIsWxAppInstalled() {
+            return jsb.reflection.callStaticMethod('MHCocosExtension', 'wechatIsWxAppInstalled');
         },
+
 
         /**
          微信分享土图片
-
          @param path 图片地址
          */
-        wechatImageShare(path) {
+        wechatImageShare: function wechatImageShare(path) {
             jsb.reflection.callStaticMethod('MHCocosExtension', 'wechatImageShare:', path);
         },
 
+
         /**
          微信链接分享
-
          @param link 链接
          @param title 标题
          @param description 说明
          */
-        wechatLinkShare(link, title, description) {
+        wechatLinkShare: function wechatLinkShare(link, title, description) {
             jsb.reflection.callStaticMethod('MHCocosExtension', 'wechatLinkShare:setTitle:setDescription:', link, title, description);
         },
 
+
         /**
          初始化oss client
-
          @param endpoint 端点
          @param accessKey 访问key
          @param secretKey 访问秘钥
          */
-        ossInit(endpoint, accessKey, secretKey) {
+        ossInit: function ossInit(endpoint, accessKey, secretKey) {
             jsb.reflection.callStaticMethod('MHCocosExtension', 'ossInit:setAccessKeyId:setSecretKey:', endpoint, accessKey, secretKey);
         },
 
+
         /**
          上传文件
-
          @param bucketName bucke 名称
          @param objectKey 存储到oss上的文件名
          @param file 本地文件路径
          */
-        ossUpload(bucketName, objectKey, file) {
+        ossUpload: function ossUpload(bucketName, objectKey, file) {
             jsb.reflection.callStaticMethod('MHCocosExtension', 'ossUpload:setObjectKey:setUrl:', bucketName, objectKey, file);
         },
+
 
         /**
          开始定位
          */
-        startLocation() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'startLocation:');
+        startLocation: function startLocation() {
+            jsb.reflection.callStaticMethod('MHCocosExtension', 'startLocation');
         },
+
 
         /**
          检查网络是否通畅
-
          @return bool
          */
-        checkNetwork() {
-            jsb.reflection.callStaticMethod('MHCocosExtension', 'checkNetwork:');
-        },
-
+        checkNetwork: function checkNetwork() {
+            return jsb.reflection.callStaticMethod('MHCocosExtension', 'checkNetwork');
+        }
     },
 
     /**
      * 导入对应平台的库文件
      */
-    init() {
+    init: function init() {
         if (cc.sys.os === cc.sys.OS_IOS) {
             this._nativeExtension = this._iOSExtension;
-        }
-        else if (cc.sys.os === cc.sys.OS_ANDROID) {
+        } else if (cc.sys.os === cc.sys.OS_ANDROID) {
             this._nativeExtension = this._androidExtension;
-        }
-        else {
+        } else {
             cc.log('window.NativeExtensionManager.execute: 不是native平台');
         }
     },
@@ -205,23 +217,26 @@ window.NativeExtensionManager = {
 
         if (!this._nativeExtension) {
             cc.log('window.NativeExtensionManager.execute: 不是native平台');
-            return;
+            return false;
         }
 
         if (!args) {
             cc.log('window.NativeExtensionManager.execute: 没有传递参数');
-            return;
+            return false;
         }
 
         if (!this._nativeExtension[name]) {
             cc.log('window.NativeExtensionManager.execute: 没有找到 ' + name + ' 方法');
-            return;
+            return false;
         }
 
-        this._nativeExtension[name].apply(null, args);
         this._listener[name] = callback;
+
+        return this._nativeExtension[name].apply(null, args);
     },
+
     callback: function callback(name, result) {
+        cc.log('NativeExtensionManager.callback.' + name + ': ' + JSON.stringify(result));
         this._listener[name](result);
         delete this._listener[name];
     }
